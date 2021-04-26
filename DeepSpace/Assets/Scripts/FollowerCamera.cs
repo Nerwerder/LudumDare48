48574e7a066d4475b3104051e3ed30d94e209af8
Sebastian <spolwind.sebastian@googlemail.com>
2021-04-27 00:15:47 +0200

    Limit Zoom (min 10, max 5000)

--- DeepSpace/Assets/Scripts/FollowerCamera.cs
+++ DeepSpace/Assets/Scripts/FollowerCamera.cs
@@ -6,6 +6,8 @@ public class FollowerCamera : MonoBehaviour
     public Transform player;
     public float smoothSpeed = 10f;
     public float zoomFactor = 3f;
+    public float maxZoom = 5000f;
+    public float minZoom = 10f;
 
     public Vector3 offset;
     Camera cam;
@@ -23,6 +25,6 @@ public class FollowerCamera : MonoBehaviour
     }
 
     public void zoom(float c) {
-        cam.orthographicSize += (-c * zoomFactor);
+        cam.orthographicSize = Mathf.Clamp((cam.orthographicSize+(-c * zoomFactor)), minZoom, maxZoom);
     }
 }
